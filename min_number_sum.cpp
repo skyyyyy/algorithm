cb2edb2d218ed021d85571cb7fa147a90f8d6945
skyyyyy <sky.heyunfei@gmail.com>
2014-12-02 13:34:22 -0800

    Create min_number_sum.cpp

---
+++ min_number_sum.cpp
@@ -0,0 +1,6 @@
+/*
+Find the Minimum Set of Numbers That Sum to at least K
+
+Problem: Given an array of integers, find a smallest subset (with minimum numbers of integers) 
+that sum to at least K. For example, a[] = {2,-5,7,8,12, 4} and K = 13, then one of the minimum set can be {8, 12}.
+*/

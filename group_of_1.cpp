57f985b6e27a5c1ca996b6d5705c28a6c7802475
skyyyyy <sky.heyunfei@gmail.com>
2014-12-04 18:12:43 -0800

    Update group_of_1.cpp

--- group_of_1.cpp
+++ group_of_1.cpp
@@ -13,4 +13,37 @@ then the output will be 2. There are two groups of "1" here.
 */
 
 int group_of_1(const vector<vector<char>> &m){
+  if(m.empty() || m[0].empty()) return 0;
+  int rows=m.size();
+  int cols =m[0].size();
+  int num_group = 0;
+  unordered_set<int> h;
+  for(int i=0;i<rows;++i){
+    for(int j=0;j<cols;++j){
+      if(m[i][j] == '1'){
+        h.insert(i*cols+j);
+      }
+    }
+  }
+  while(!h.empty()){
+     int val=*h.begin();
+     unordered_set<int> g;
+     g.insert(val);
+     queue<int> q;
+     q.push(val);
+     while(!q.empty()){
+       int k = q.front();
+       q.pop();
+       int r = k/cols;
+       int c = k%col;
+       if(r>0 && g.find((r-1)*cols+c) == g.end() ){ g.insert((r-1)*cols+c); q.push((r-1)*cols+c);}
+       if(r<rows-1 && g.find((r+1)*cols+c) == g.end()){ g.insert((r+1)*cols+c);q.push((r+1)*cols+c);}
+       if(c>0 && g.find(r*cols+c-1)) == g.end()){ g.insert(r*cols+c-1); q.push(r*cols+c-1);}
+       if(c<cols-1 && g.find(r*cols+c+1) == g.end()){g.insert(r*cols+c+1); q.push(r*cols+c+1);}
+     }
+     for(unordered_set::iterator it=g.begin(); it!=g.end(); ++it)  h.erase(*it);
+     
+     ++num_group;
+  }
+  return num_group;
 }

097b213ba5aa18096b5d6634da2a798abe324f63
skyyyyy <sky.heyunfei@gmail.com>
2014-12-04 17:36:12 -0800

    Create group_of_1.cpp

---
+++ group_of_1.cpp
@@ -0,0 +1,16 @@
+/*
+Find the Number of Groups of "1" in a Matrix
+Problem: Given a matrix, each cell of the matrix is either "1" or "0". Consider that all the adjacent "1" form a group,
+find out how many such groups are in the matrix.  By"adjacent",  we mean a cell's left, right, up and down. 
+The diagonal is not considered as "adjacent". For example, if we have a matrix as follow:
+
+1 0 0 1
+1 1 0 1
+0 1 1 1
+1 0 0 0
+
+then the output will be 2. There are two groups of "1" here.
+*/
+
+int group_of_1(const vector<vector<char>> &m){
+}

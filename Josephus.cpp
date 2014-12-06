b0e1c106ed1ec2cd86c980f7a50699a1dd1dc55c
skyyyyy <sky.heyunfei@gmail.com>
2014-12-05 17:09:04 -0800

    Update Josephus.cpp

--- Josephus.cpp
+++ Josephus.cpp
@@ -7,3 +7,40 @@ then we eliminate 1 and 5, so the last person's index is 3. So, F(5, 2) = 3
 
 Try to get F(n, k)
 */
+structure LinkNode{
+  int val;
+  LinkNode* next;
+  LinkNode(int x):val(x),next(NULL){}
+};
+
+int f(int n,int k){
+  if(n<=0) return -1;
+  if(n<=0) return -1;
+  LinkNode* p1=new LinkNode(1);
+  LinkNode* p2=p1;
+  for(int i=2;i<=n;++i){
+    LinkNode* p = new LinkNode(i);
+    p2->next = p;
+    p2 = p;
+  }
+  p2->next = p1;
+  
+  int i=0;
+  while(true){
+    LinkNode* next = p1->next;
+    if(p1 == next){
+      int val = p1->val;
+      delete p1;
+      return val;
+    }else if(i==k%n){
+      p1->next = next->next;
+      delete next;
+      i=1;
+      p1 = p1->next;
+    }else{
+      ++i;
+      p1 = next;
+    }
+  }
+  return -1;
+}

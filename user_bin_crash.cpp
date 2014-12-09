2f9117a035831b24e451835fc3a12154da035657
skyyyyy <sky.heyunfei@gmail.com>
2014-12-09 00:46:53 -0800

    Create user_bin_crash.cpp

---
+++ user_bin_crash.cpp
@@ -0,0 +1,8 @@
+/*
+User Bin Crash problem
+Problem: This is a problem from facebook buzz. The problem is that a plane is gong to crash,  
+you need to throw out W lbs of freight to survive the crash. Each type of freight fi is associated with a weight wi and price pi.
+If we throw one unit of fi out, we lost pi but the plane becomes lighter by wi. 
+Our mission is to throw out minmum freights measured by their aggregate price and avoid the crash, 
+which means the total weight of those freights should be larger than W.
+*/

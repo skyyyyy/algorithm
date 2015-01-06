38729c8893b9c9706e9888583eb3e3e44acd268d
skyyyyy <sky.heyunfei@gmail.com>
2015-01-05 16:44:17 -0800

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -2,7 +2,7 @@
 
 //time:n^2, space:n^2
 int knapsack(const vector<int> &vals, const vector<int> &weights, int weight_limit){
-   vector<vector<int>> dp(vals.size()+1, vector<int>(weight_.imit+1,0));
+   vector<vector<int>> dp(vals.size()+1, vector<int>(weight_limit+1,0));
    for(int i=1;i<=vals.size();++i){
      for(int j=1;j<=weight_limit;++j){
        dp[i][j] = d[i-1][j];

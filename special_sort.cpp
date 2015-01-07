3cb0797fab55b94ea078b6f2045ef75ca9f4e778
skyyyyy <sky.heyunfei@gmail.com>
2015-01-06 16:12:21 -0800

    Update special_sort.cpp

--- special_sort.cpp
+++ special_sort.cpp
@@ -33,3 +33,56 @@ void SpecialSort(int* arr, int n)
             swap(arr[negIndex], arr[posIndex++]);
     }
 }
+
+/*above function doesn't work for below case
+int[] a = {1, -1, 3, -2, -4, 2};
+法交换出的结果是
+-2, -1, -4, 1, 3, 2, 
+
+原有顺序木有保持.
+*/
+
+//here is nlog(n) approach
+ public void oneZeroSort(int[] a) {
+        int negCount = helper(a, 0, a.length - 1);
+        for (int i = 0; i < a.length; i ++) {
+            if (i < negCount) {
+                a[i] = - Math.abs(a[i]);
+            } else {
+                a[i] = Math.abs(a[i]);
+            }
+        }
+    }
+
+    int helper(int[] a, int l, int r) {
+        if (l >= r) return 0;
+
+        int negCount = 0;
+        for (int i = l; i <= r; i ++) {
+            if (a[i] < 0) negCount ++;
+        }
+
+        int negIndex = l;
+        int posIndex = l + negCount;
+        boolean isSwitch = false;
+        while (negIndex < l + negCount && posIndex <= r) {
+            if (a[negIndex] > 0 && a[posIndex] < 0) {
+                int tmp = a[negIndex];
+                a[negIndex] = - a[posIndex];
+                a[posIndex] = - tmp;
+                posIndex ++;
+                negIndex ++;
+                isSwitch = true;
+            } else {
+                if (a[negIndex] < 0) negIndex++;
+                if (a[posIndex] > 0) posIndex++;
+            }
+        }
+
+        if (isSwitch) {
+            helper(a, l, negCount - 1);
+            helper(a, l + negCount, r);
+        }
+
+        return negCount;
+    }

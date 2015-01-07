d1d2a938a1614b5b2c97007d69a8b70008d96230
skyyyyy <sky.heyunfei@gmail.com>
2015-01-07 15:00:15 -0800

    Create min_vertex_cover.cpp

---
+++ min_vertex_cover.cpp
@@ -0,0 +1,6 @@
+/*
+Problem: Given a tree, find its minimum vertex cover. 
+Wait, what is a vertex cover? Given a undirected graph G(V,E),  
+a vertex cover is a subset of V such that for any egdee in E, at least one of e's two endpoints 
+should be in this subset (vertex cover).
+*/

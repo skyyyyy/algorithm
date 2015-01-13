155e76744d79503faa26141d344dba94c6c61b7a
skyyyyy <sky.heyunfei@gmail.com>
2015-01-13 14:24:31 -0800

    Update topological_sort.cpp

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -1,4 +1,34 @@
 /*
+Given an directed graph, a topological order of the graph nodes is defined as follow:
+
+For each directed edge A-->B in graph, A must before B in the order list.
+The first node in the order can be any node in the graph with no nodes direct to it.
+Find any topological order for the given graph.
+Note
+You can assume that there is at least one topological order in the graph.
+Example
+For graph as follow: 
+
+
+
+The topological order can be:
+
+[0, 1, 2, 3, 4, 5]
+
+or
+
+[0, 2, 3, 1, 5, 4]
+
+or
+
+....
+
+
+
+Challenge
+Can you do it in both BFS and DFS?
+*/
+/*
 bfs
 */
 

ae5cb6b15107e4697aba6517b16be8a3ff7ad357
skyyyyy <sky.heyunfei@gmail.com>
2015-01-13 16:33:57 -0800

    Update closest_leaf.cpp

--- closest_leaf.cpp
+++ closest_leaf.cpp
@@ -16,3 +16,18 @@ Closest leaf to 'H' is 'K', so distance is 1 for 'H'
 Closest leaf to 'C' is 'B', so distance is 2 for 'C'
 Closest leaf to 'E' is either 'I' or 'J', so distance is 2 for 'E' 
 Closest leaf to 'B' is 'B' itself, so distance is 0 for 'B' 
+
+int closestLeaf(TreeNode* root, TreeNode* target){
+    int min_dist = INT_MAX;
+    minDistToLeaf(root, target,min_dist);
+    return min_dist;
+}
+int minDistToLeaf(TreeNode* root, TreeNode* target, int &min_dist){
+    if(!root) return INT_MAX;
+    int res = 0;
+    if(!root->left && !root->right) res=1;
+    else res = 1 +min(minDistToLeaf(root->left, target,min_dist),minDistToLeaf(root->right,target,min_dist));
+    if(target == root) min_dist = min(min_dist, res);
+    else if(target == root->left || target==root->right) min_dist=min(min_dist,res+1);
+    return res;
+}

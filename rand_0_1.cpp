85b32d3fda2ca7652e2506350fd7a9a270b1d7b1
skyyyyy <sky.heyunfei@gmail.com>
2015-01-15 14:43:31 -0800

    Update rand_0_1.cpp

--- rand_0_1.cpp
+++ rand_0_1.cpp
@@ -4,4 +4,10 @@ that can return 0 with 90%, and 1 with 10%.
 */
 
 int random_new(){
+  while(true){
+    int res = 0;
+    for(int i=0;i<10;++i) res |= random()<<i;
+    if(res<=99) return 1;
+    else if(res<=999) return 0;
+  }
 }

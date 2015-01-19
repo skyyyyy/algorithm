09372ca5acc852746f671c007b2880d64b52ce08
skyyyyy <sky.heyunfei@gmail.com>
2015-01-19 00:41:43 -0800

    Create evaluate_string.cpp

---
+++ evaluate_string.cpp
@@ -0,0 +1,3 @@
+Parse a formula string (only contains "+-()", no "*/"), for example,   “
+5 + 2x – ( 3y + 2x  - ( 7 – 2x) – 9 ) = 3 + 4y”, parse this string, with
+a given float of ‘x’ value, output a float for ‘y’ value.

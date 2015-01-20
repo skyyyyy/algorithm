db2831ab245b0306dd30620e58c65c804df95a6b
skyyyyy <sky.heyunfei@gmail.com>
2015-01-20 15:28:58 -0800

    Update H2O.cpp

--- H2O.cpp
+++ H2O.cpp
@@ -1,3 +1,67 @@
 实现两个函数: H() and O(), 这两个函数会被多线程调用。当一个线程调用H或O时
 ，如果当前已经有至少两个线程call H和一个线程call O。那么让两个call H和一个
 call O的线程返回（产生一个水分子），其他的都block
+
+lock_guard and unique_lock:
+  The difference is that you can lock and unlock a std::unique_lock. 
+  std::lock_guard will be locked only once on construction and unlocked on destruction.
+  
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+ 
+std::mutex m;
+std::condition_variable cv;
+std::string data;
+bool ready = false;
+bool processed = false;
+ 
+void worker_thread()
+{
+    // Wait until main() sends data
+    std::unique_lock<std::mutex> lk(m);
+    cv.wait(lk, []{return ready;});
+ 
+    // after the wait, we own the lock.
+    std::cout << "Worker thread is processing data\n";
+    data += " after processing";
+ 
+    // Send data back to main()
+    processed = true;
+    std::cout << "Worker thread signals data processing completed\n";
+ 
+    // Manual unlocking is done before notifying, to avoid waking up
+    // the waiting thread only to block again (see notify_one for details)
+    lk.unlock();
+    cv.notify_one();
+}
+ 
+int main()
+{
+    std::thread worker(worker_thread);
+ 
+    data = "Example data";
+    // send data to the worker thread
+    {
+        std::lock_guard<std::mutex> lk(m);
+        ready = true;
+        std::cout << "main() signals data ready for processing\n";
+    }
+    cv.notify_one();
+ 
+    // wait for the worker
+    {
+        std::unique_lock<std::mutex> lk(m);
+        cv.wait(lk, []{return processed;});
+    }
+    std::cout << "Back in main(), data = " << data << '\n';
+ 
+    worker.join();
+}  
+Output:
+main() signals data ready for processing
+Worker thread is processing data
+Worker thread signals data processing completed
+Back in main(), data = Example data after processing

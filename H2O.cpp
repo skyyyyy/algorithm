f97e227d129a457e8f723e9548409e5bf44b70e1
skyyyyy <sky.heyunfei@gmail.com>
2015-01-20 17:15:29 -0800

    Update H2O.cpp

--- H2O.cpp
+++ H2O.cpp
@@ -65,3 +65,65 @@ main() signals data ready for processing
 Worker thread is processing data
 Worker thread signals data processing completed
 Back in main(), data = Example data after processing
+
+
+Here is the final code for a simple thread-safe multiple producer, multiple consumer queue:
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+ 
+template <typename T>
+class Queue
+{
+ public:
+ 
+  T pop()
+  {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (queue_.empty())
+    {
+      cond_.wait(mlock);
+    }
+    auto item = queue_.front();
+    queue_.pop();
+    return item;
+  }
+ 
+  void pop(T& item)
+  {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (queue_.empty())
+    {
+      cond_.wait(mlock);
+    }
+    item = queue_.front();
+    queue_.pop();
+  }
+ 
+  void push(const T& item)
+  {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    queue_.push(item);
+    mlock.unlock();
+    cond_.notify_one();
+  }
+ 
+  void push(T&& item)
+  {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    queue_.push(std::move(item));
+    mlock.unlock();
+    cond_.notify_one();
+  }
+ 
+ private:
+  std::queue<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable cond_;
+};
+
+
+
+

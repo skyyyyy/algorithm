d2b90ac0b75453989b417b5eb4479214e021e851
skyyyyy <sky.heyunfei@gmail.com>
2015-07-24 14:58:27 -0700

    Update three_increasing.cpp

--- three_increasing.cpp
+++ three_increasing.cpp
@@ -3,8 +3,8 @@ give an integer array A, find out if there is a triplet index, i,j and k, which 
 */
 //linear time, without extra space
 bool find3Numbers(int A[], int n){
-   int first = INT_MIN;
-   int second = INT_MIN;
+   int first = INT_MAX;
+   int second = INT_MAX;
    for(int i=0;i<n;++i){
        if(A[i]<=first) first = A[i];
        else if(A[i]<=second) second = A[i];
